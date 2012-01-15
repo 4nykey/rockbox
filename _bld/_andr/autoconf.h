e574000ebfad7c418ced91479fa66606e9d0aa92
ak <4nykey@gmail.com>
2012-01-15 19:48:10 +0200

    adding build dir for android

---
+++ _bld/_andr/autoconf.h
@@ -0,0 +1,40 @@
+/* This header was made by configure */
+#ifndef __BUILD_AUTOCONF_H
+#define __BUILD_AUTOCONF_H
+
+/* Define endianess for the target or simulator platform */
+#define ROCKBOX_LITTLE_ENDIAN 1
+
+/* Define this if you build rockbox to support the logf logging and display */
+#undef ROCKBOX_HAS_LOGF
+
+/* Define this to record a chart with timings for the stages of boot */
+#undef DO_BOOTCHART
+
+/* optional define for a backlight modded Ondio */
+
+
+/* optional define for FM radio mod for iAudio M5 */
+
+
+/* optional define for ATA poweroff on Player */
+
+
+/* optional defines for RTC mod for h1x0 */
+
+
+
+/* the threading backend we use */
+#define ASSEMBLER_THREADS
+
+/* lcd dimensions for application builds from configure */
+#define LCD_WIDTH 480
+#define LCD_HEIGHT 800
+
+/* root of Rockbox */
+#define ROCKBOX_DIR "/.rockbox"
+#define ROCKBOX_SHARE_PATH "/data/data/org.rockbox/app_rockbox/rockbox"
+#define ROCKBOX_BINARY_PATH "/data/data/org.rockbox/lib"
+#define ROCKBOX_LIBRARY_PATH "/data/data/org.rockbox/app_rockbox"
+
+#endif /* __BUILD_AUTOCONF_H */
